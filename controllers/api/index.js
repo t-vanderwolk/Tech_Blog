b50968f1ec438433db922b400674745019feb3ce
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-05-01 10:43:16 -0700

    added handlebars. views, layouts and partials. seeded database and app running

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
-
-// router.use((req, res) => {
-//   res.status(404).end();
-// });
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
