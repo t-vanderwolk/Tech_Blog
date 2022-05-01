4d6953253929e64d14878e276717dcdc170d7398
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-05-01 11:37:20 -0700

    saw how db processes data

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,14 +3,17 @@ const { User } = require('../models');
 const userdata =
 [
   {
+    "userId" : 1,
     "username": "TayTay",
     "password": "password"
   },
   {
+    "userId": 2,
     "username": "Em",
     "password": "password"
   },
   {
+    "userId": 3,
     "username": "Ava",
     "password": "password"
   }
