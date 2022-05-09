7dc40fc70ca26db123d07a80bdd6b15ce43246b9
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-05-09 15:55:52 -0700

    changed around ids and usernames to make corrrect posts match user

--- seeds/postData.js
+++ seeds/postData.js
@@ -3,19 +3,25 @@ const { Post } = require('../models');
 const postdata =
 [
   {
+    "userId": 1,
+    "username": "TayTay",
     "postTitle": "Bootcamp",
-    "postContent": "Im so ready to be done with this course!",
-    "userId": 1
+    "postContent": "Im so ready to be done with this course!"
+    
   },
   {
+    "userId": 2,
+    "username": "Em",
     "postTitle": "REMINDERS",
-    "postContent": "1.Eat real food. 2.Drink water. 3.Walk Brinx. 4.Code",
-    "userId": 2
+    "postContent": "1.Eat real food. 2.Drink water. 3.Walk Brinx. 4.Code"
+ 
   },
   {
+    "userId":3,
+    "username": "Ava",
     "postTitle": "New Mom Life",
-    "postContent": "My baby doesnt even appriciate me!",
-    "userId": 3
+    "postContent": "My baby doesnt even appriciate me!"
+   
   }
 ];
 
